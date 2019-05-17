ae37c7fd1d43a787fdf32d8acc9c599f2c8b15b5
iet-konczol <49302909+iet-konczol@users.noreply.github.com>
2019-05-17 13:58:12 +0200

    Add files via upload

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,18 +8,28 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  private TorpedoStore mockDA1 = new TorpedoStore(10);
+  private TorpedoStore mockDA2 = new TorpedoStore(10);
+
+
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+
+    mockDA1 = mock(TorpedoStore.class);
+    mockDA2 =mock(TorpedoStore.class);
+    this.ship = new GT4500(mockDA1,mockDA2);
+
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    this.ship = ship;
     // Act
+
+
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
